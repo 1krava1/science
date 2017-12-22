62c41b6ca3b39b0dcc13994e20d72467acbc41a3
1krava1 <krava9519@gmail.com>
2017-12-22 16:11:00 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,73 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainComponent} from './components/main/main.component';
+import {EditComponent} from './components/edit/edit.component';
+import {MapComponent} from './components/map/map.component';
+import {PlaceComponent} from './components/place/place.component';
+import {PlacesComponent} from './components/places/places.component';
+import {UserComponent} from './components/user/user.component';
+import {UsersComponent} from './components/users/users.component';
+import {RegisterComponent} from './components/register/register.component';
+import {LoginComponent} from './components/login/login.component';
+import {EditUserComponent} from './components/edit-user/edit-user.component';
+import {EditPlaceComponent} from './components/edit-place/edit-place.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'map',
+    component: MapComponent,
+  },
+  {
+    path: 'edit',
+    component: EditComponent,
+    children: [
+      {
+        path: 'user/:id',
+        component: EditUserComponent
+      },
+      {
+        path: 'place/:id',
+        component: EditPlaceComponent
+      },
+      {
+        path: 'user',
+        component: EditUserComponent
+      },
+      {
+        path: 'place',
+        component: EditPlaceComponent
+      },
+    ]
+  },
+  {
+    path: 'place/:id',
+    component: PlaceComponent,
+  },
+  {
+    path: 'places',
+    component: PlacesComponent,
+  },
+  {
+    path: 'user/:id',
+    component: UserComponent,
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { MapComponent } from './components/map/map.component';
+import { EditComponent } from './components/edit/edit.component';
+import { PlaceComponent } from './components/place/place.component';
+import { PlacesComponent } from './components/places/places.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { EditPlaceComponent } from './components/edit-place/edit-place.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MapComponent,
+    EditComponent,
+    PlaceComponent,
+    PlacesComponent,
+    UserComponent,
+    UsersComponent,
+    RegisterComponent,
+    LoginComponent,
+    EditUserComponent,
+    EditPlaceComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
